1edc4822b70e7cafdc99bae336670e5dbcebf3e2
b_ly <ylb85312574@163.com>
2024-10-23 00:50:57 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="">我是App</div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const a = ref(2)
+console.log(a)
+</script>
+
+<style lang="scss" scoped></style>
